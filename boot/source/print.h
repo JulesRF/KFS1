9c0409a78150296f17225741b42d198b1b515479
Robin DEL AGUILA <delaguilarobin5@gmail.com>
2024-03-05 15:53:26 +0100

    addingColors

--- boot/source/print.h
+++ boot/source/print.h
@@ -5,10 +5,21 @@
 # define VGA_ADDRESS 0xB8000
 
 # define BLACK 0
+# define BLUE 1
 # define GREEN 2
+# define CYAN 3
 # define RED 4
+# define MAGENTA 5
+# define BROWN 6
+# define L_GREY 7
+# define D_GREY 8
+# define L_BLUE 9
+# define L_GREEN 10
+# define L_CYAN 11
+# define L_RED 12
+# define L_MAGENTA 13
 # define YELLOW 14
-# define WHITE_COLOR 15
+# define WHITE 15
 
 void clear_screen(void);
 void print_string(char* str, unsigned char color);

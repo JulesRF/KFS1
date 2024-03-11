1bd207ee77c0464bf2833ed8382fc46f52e1856f
JulesRF <jules.rf.jrf@gmail.com>
2024-03-11 18:00:27 +0100

    added a new terminal index

--- boot/source/kernel.c
+++ boot/source/kernel.c
@@ -2,21 +2,21 @@
 
 int	main(void)
 {
-	terminal_buffer = (unsigned short*)VGA_ADDRESS;
-	vga_index = 0;
+	terminal_buffer[0] = (unsigned short*)VGA_ADDRESS;
+	vga_index[0] = 0;
 
-	clear_screen();
-	clear_screen();
-	clear_screen();
+	clear_screen(0);
+	clear_screen(0);
+	clear_screen(0);
 
-	print_string("Hello\n", L_MAGENTA);
-    print_string("World", L_CYAN);
-    print_string("!", GREEN);
+	print_string("Hello\n", L_MAGENTA, 0);
+    print_string("World", L_CYAN, 0);
+    print_string("!", GREEN, 0);
     // vga_index = 160;
-    print_char('4', RED);
-    print_char('2', YELLOW);
-    print_char('0' + ft_strlen("salut"), RED);
-    ft_printerr("\nError\nSegfault");
+    print_char('4', RED, 0);
+    print_char('2', YELLOW, 0);
+    print_char('0' + ft_strlen("salut"), RED, 0);
+    ft_printerr("\nError\nSegfault", 0);
     // vga_index = 240;
 
     shell_ter();
--- boot/source/print.c
+++ boot/source/print.c
@@ -1,50 +1,52 @@
 #include "include/kfs.h"
 
-unsigned short *terminal_buffer;
-unsigned int    vga_index;
+unsigned short *terminal_buffer[2];
+unsigned int    vga_index[2];
 
-void clear_screen(void)
+void clear_screen(int terminal_index)
 {
     int index = 0;
     /* there are 25 lines each of 80 columns;
        each element takes 2 bytes */
-    while (index < 80 * 25 * 2) {
-        terminal_buffer[index] = ' ';
+    while (index < 80 * 25 * 2)
+    {
+        terminal_buffer[terminal_index][index] = ' ';
         index += 1; //TODO modified from 2, was not working with the make kernel
     }
 }
 
-void print_string(char* str, unsigned char color)
+void print_string(char* str, unsigned char color, int terminal_index)
 {
     int index = 0;
-    while (str[index]) {
-        ft_isnewl(str, index);
-        terminal_buffer[vga_index] = (unsigned short)str[index] | (unsigned short)color << 8;
+    while (str[index])
+    {
+        ft_isnewl(str, index, terminal_index);
+        terminal_buffer[terminal_index][vga_index[terminal_index]] = (unsigned short)str[index] | (unsigned short)color << 8;
         index++;
-        vga_index++;        
+        vga_index[terminal_index]++;
     }
 }
 
-void print_char(char str, unsigned char color)
+void print_char(char str, unsigned char color, int terminal_index)
 {
     int index = 0;
     
-    terminal_buffer[vga_index] = str | (unsigned short)color << 8;
+    terminal_buffer[terminal_index][vga_index[terminal_index]] = str | (unsigned short)color << 8;
     index++;
-    vga_index++;
+    vga_index[terminal_index]++;
 }
 
-int ft_isnewl(const char *str, int i)
+int ft_isnewl(const char *str, int i, int terminal_index)
 {
     if (str[i] == '\n')
     {
-        vga_index = vga_index + 80 - (vga_index % 80) - 1;
+        vga_index[terminal_index] = vga_index[terminal_index] + 80 - (vga_index[terminal_index] % 80) - 1;
         return (1);
     }
     return (0);
 }
 
-void    ft_printerr(char *str)
+void    ft_printerr(char *str, int terminal_index)
 {
-    print_string(str, RED);
+    print_string(str, RED, terminal_index);
 }

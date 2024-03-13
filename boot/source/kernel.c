d2b16200cd5399c722c993bc249d0468b2aa5cca
JulesRF <jroux-fo@BESS-f2r4s8.clusters.42paris.fr>
2024-03-13 17:58:36 +0100

    tried fixing swith_screen

--- boot/source/kernel.c
+++ boot/source/kernel.c
@@ -4,36 +4,37 @@ int	main(void)
 {
 	screen = 1;
 	terminal_buffer[0] = (unsigned short*)VGA_ADDRESS;
+	vga_index[0] = 0;
 	vga_index[1] = 0;
 	vga_index[2] = 0;
 
 	clear_screen(0);
-	clear_screen(0);
-	clear_screen(0);
+	clear_screen(1);
+	clear_screen(2);
 
 	print_string("Hello\n", L_MAGENTA);
     print_string("World", L_CYAN);
     print_string("!", GREEN);
-    // vga_index = 160;
-    print_char('4', RED);
+	ft_switch_screen();
+	print_char('4', RED);
     print_char('2', YELLOW);
 	ft_putnbr_hex(42, WHITE);
 	ft_putnbr(42, WHITE);
     print_char('0' + ft_strlen("salut"), RED);
     print_string("\n", WHITE);
-	// ft_switch_screen();
-	// ft_switch_screen();
-    // vga_index = 240;
 
-    // shell_ter();
+	// ft_switch_screen();
+	// print_string("Oh que oui", WHITE);
 
-    print_string("Installing interrupt service routines (ISRs).\n", WHITE);
+
+
+    // print_string("Installing interrupt service routines (ISRs).\n", WHITE);
     isr_install();
 
-    print_string("Enabling external interrupts.\n", WHITE);
+    // print_string("Enabling external interrupts.\n", WHITE);
     asm volatile("sti");
 
-    print_string("Initializing keyboard (IRQ1).\n", WHITE);
+    // print_string("Initializing keyboard (IRQ1).\n", WHITE);
     init_keyboard();
     return 0 ;
 }
